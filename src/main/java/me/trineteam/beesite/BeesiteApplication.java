186d437059dc751297a1d0dc0c576d146be43e91
mooejun <mooejun@163.com>
2019-04-18 17:06:27 +0800

    :tada: 创建项目

---
+++ src/main/java/me/trineteam/beesite/BeesiteApplication.java
@@ -0,0 +1,13 @@
+package me.trineteam.beesite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeesiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeesiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/trineteam/beesite/BeesiteApplicationTests.java
@@ -0,0 +1,16 @@
+package me.trineteam.beesite;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BeesiteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
